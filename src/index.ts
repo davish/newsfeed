2ba9f991770d9287e0fdbc09c928feb1d2912175
Davis Haupt <davis.haupt@gmail.com>
2025-11-30 22:09:50 -0500

    fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
-import RSSParser from "rss-parser";
-async function main() {
-  console.log("RSS Feed");
-  const parser = new RSSParser();
-  const feed = await parser.parseURL('https://www.reddit.com/r/rust.rss');
-  console.log(feed.title);
-}
+async function main() {}
 
 main();
